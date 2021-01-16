956912e8ffc43eccf6b427c330fe5011935e0267
Maksym Rakomin <maksym.rakomin@gmail.com>
2021-01-16 19:31:35 +0200

    created a structure for globally adding event listeners

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,14 @@
 import {DomListener} from '@core/DomListener';
 
 export class ExcelComponent extends DomListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners);
+  }
   // Возвращает шаблон компонента
   toHTML() {
     return ''
+  }
+  init() {
+    this.initDOMListeners()
   }
 }
