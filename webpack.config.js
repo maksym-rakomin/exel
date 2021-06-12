3c1998574124de5e1342f72d0a799bbff40afe05
Maksym Rakomin <maksym.rakomin@gmail.com>
2021-06-12 06:34:27 +0300

    add DefinePlugin to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const ESLintPlugin = require('eslint-webpack-plugin')
+const webpack = require('webpack')
 
 const isProd = process.env.NODE_ENV === 'production'
 const isDev = !isProd
@@ -54,6 +55,9 @@ module.exports = {
       filename: fileName('css'),
     }),
     new ESLintPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
   ],
   module: {
     rules: [
