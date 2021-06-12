d802a7699cb97b438f12e9332c395cbcab6a7ca1
Maksym Rakomin <maksym.rakomin@gmail.com>
2021-06-12 05:46:31 +0300

    created routing between main page and table

---
+++ src/pages/dashboard.functions.js
@@ -0,0 +1,44 @@
+import {storage} from '@core/utils';
+
+export function toHTML(key) {
+  const state = storage(key)
+  const id = key.split(':')[1]
+  return `
+      <li class="db__record">
+        <a href="#excel/${id}">${state.title}</a>
+        <strong>12.06.2020</strong>
+      </li>
+     `
+}
+
+function getAllKeys() {
+  const keys =[]
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i)
+
+    if (!key.includes('excel')) {
+      continue
+    }
+    keys.push(key)
+  }
+  return keys
+}
+
+export function createRecordsTable() {
+  const keys = getAllKeys()
+
+  if (keys.length === 0) {
+    return `<p>Пока не создано таблиц</p>`
+  }
+
+  return `
+      <div class="db__list-header">
+        <span>Название</span>
+        <span>Дата открытия</span>
+      </div>
+
+      <ul class="db__list">
+        ${ keys.map(toHTML).join('') }
+      </ul>
+  `
+}
--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,5 +1,5 @@
-import {storage} from '@core/utils';
 import {defaultStyles, defaultTitle} from '@/constants';
+import {cloneObj} from '@core/utils';
 
 const defaultState = {
   title: defaultTitle,
@@ -17,6 +17,6 @@ const normalize = state => ({
   currentState: '',
 })
 
-export const initialState = storage('excel-state') ?
-  normalize(storage('excel-state')) :
-  defaultState
+export function normalizeInitialState(state) {
+  return state ? normalize(state) : cloneObj(defaultState)
+}
